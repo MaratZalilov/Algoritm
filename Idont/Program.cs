64c16159b04b722de5a7a8abd16c2d30a3aecb33
студент2 <2@DESKTOP-QEFI0G7>
2022-10-29 11:34:51 +0300

    Добавьте файлы проекта.

---
+++ Idont/Program.cs
@@ -0,0 +1,18 @@
+﻿using Idont;
+
+Algoritm algoritm = new Algoritm();
+Console.WriteLine("Введите число");
+string count1= Console.ReadLine();
+bool parse=int.TryParse(count1,out var count);
+if (!parse)
+{
+    Console.WriteLine("Ошибка");
+    return;
+}
+Console.WriteLine();
+Console.WriteLine("Введите строку");
+string enter=Console.ReadLine();
+if(parse) 
+{
+    algoritm.funk(count, enter);
+}
---
+++ Idont/algoritm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idont
+{
+    public class Algoritm
+    {   
+        public void funk(int count, string words)
+        {   
+            for (int i = 0; i < count ; count--)
+            {
+                //System.Threading.Thread.Sleep(1000);
+               
+                Console.Write($"{words}");
+            }
+        }
+    }
+}
